c96531318588b6cb549f263f12fee0e03a4ae793
Balibabu <balibabuchauhan1@gmail.com>
2026-05-03 15:51:05 +0545

    fixed enc keys

--- git/extra/githubManager.py
+++ git/extra/githubManager.py
@@ -1,7 +1,7 @@
 from github import Github
 import base64
 from .config import Configurations
-from .crypto import encrypt_chunk, decrypt_chunk
+from .crypto import encrypt_chunk, decrypt_chunk, derive_chunk_key
 
 class GithubManager:
     def __init__(self,username) -> None:
@@ -11,7 +11,8 @@ class GithubManager:
     def upload_file(self,fileContent,filename,repo_name):
         repo = self.git.get_repo("storeage/"+repo_name)
         file_path_in_repo=f'{self.username}/{filename}'
-        enc=encrypt_chunk(fileContent, Configurations.MASTER_KEY)
+        key = derive_chunk_key(Configurations.MASTER_KEY, repo_name, filename)
+        enc=encrypt_chunk(fileContent, key)
         repo.create_file(
             path=file_path_in_repo,
             message='uploaded '+filename,
@@ -23,7 +24,8 @@ class GithubManager:
         file = repo.get_contents(f'{self.username}/{filename}')
         blob_content = repo.get_git_blob(file.sha).content
         content_bytes = base64.b64decode(blob_content)
-        return decrypt_chunk(content_bytes, Configurations.MASTER_KEY)
+        key = derive_chunk_key(Configurations.MASTER_KEY, repo_name, filename)
+        return decrypt_chunk(content_bytes, key)
 
     def create_repo(self,repo_name):
         user=self.git.get_user()
